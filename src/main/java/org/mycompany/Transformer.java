f8f9c0fdeae0c418ae01b65dd78425ec6435b30b
Rob Dingwell <rob@robdingwell.org>
2017-04-11 11:00:38 -0400

    removing old files

--- src/main/java/org/mycompany/Transformer.java
+++ src/main/java/org/mycompany/Transformer.java
@@ -8,12 +8,14 @@ import org.apache.camel.Processor;
 
 public class Transformer implements Processor {
 	public void process(Exchange exchange) throws Exception {
-        System.out.println("MyProcessor started");
         Map myMap = exchange.getIn().getBody(Map.class);
         Message msg = exchange.getIn();
         msg.setHeaders(myMap);
         msg.setBody(myMap.get("payloadTextContent"));
-        System.out.println("MyProcessor complete");
+        msg.setHeader("SOURCE", "PHINMS");
+        msg.setHeader("SOURCE_ID", myMap.get("recordId"));
+        msg.setHeader("ID", "PHINMS_"+myMap.get("recordId"));
+   
     }
 }
 
