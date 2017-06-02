01ef845321519b2879e031c8c4946a5a19e8b3da
Kevin Hennessy <khennessy@mitre.org>
2017-06-02 10:40:29 -0400

    Improved unit test

--- src/test/java/org/cdc/gov/sdp/aphl/AphlS3ComponentTest.java
+++ src/test/java/org/cdc/gov/sdp/aphl/AphlS3ComponentTest.java
@@ -4,11 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
@@ -49,44 +47,54 @@ public class AphlS3ComponentTest {
 	@Produce(uri = "direct:aphls3")
 	protected ProducerTemplate template;
 
+	private final String accessKey = "accessKey1";
+	private final String secretKey = "verySecretKey1";
+	private final String bucketName = "tradingpartners-east.sandbox.aimsplatform.com";/// AIMSPlatform
+	private final String s3url = "http://127.0.0.1:8000";
+
+	private final String file_name = "hl7v2.txt";
+	private final String input_file_path = "src/test/resources/" + file_name;
+	private final String output_file_path = "test/" + file_name;
+
+	private final String S3_KEY_HEADER = "CamelAwsS3Key";
+	private final String s3ObjectKey = "TRY";
+
 	@Test
 	@DirtiesContext
 	public void testAphlS3Route() throws Exception {
-		final String accessKey = "accessKey1";
-		final String secretKey = "verySecretKey1";
-		final String bucketName = "tradingpartners-east.sandbox.aimsplatform.com";/// AIMSPlatform
-		final String s3url = "http://127.0.0.1:8000";
-
-		final String file_name = "hl7v2.txt";
-
-		final String source_file_path = "src/test/resources/hl7v2.txt";
-		final String input_file_path = "test/" + file_name;
-		final String output_file_path = "test2/" + file_name;
-
-		final String s3ObjectKey = "TRY";
-
-		// Copy known test file to test route input folder
-		if (!Files.exists(Paths.get(input_file_path))) {
-			Files.copy(Paths.get(source_file_path), Paths.get(input_file_path));
-		}
-
 		// Set content expectations
-		List<String> expected_lines = Files.readAllLines(Paths.get(input_file_path));
+		String expected_content = readFile(input_file_path);
+		foo.expectedMessageCount(1);
 
+		// Build content
+		Exchange exchange = new DefaultExchange(camelContext);
+		Message msg = new DefaultMessage();
+
+		msg.setHeader(S3_KEY_HEADER, s3ObjectKey);
+		msg.setBody(expected_content);
+		exchange.setIn(msg);
+
+		// Send content
+		template.send(exchange);
+
+		// Confirm it was delivered
+		MockEndpoint.assertIsSatisfied(camelContext);
+
+		// Now to confirm it was delivered correctly...
 		// Establish connection to s3 instance (mock or legitimate)
 		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 		AmazonS3 s3client = new AmazonS3Client(credentials);
 		s3client.setEndpoint(s3url);
 
-		// Get object and put in 2nd test directory to easily examine it
+		// Get object and put in a test directory to easily examine it
 		s3client.getObject(new GetObjectRequest(bucketName, s3ObjectKey), new File(output_file_path));
 		assertTrue(Files.exists(Paths.get(output_file_path)));
 
-		// Easily examine it
-		List<String> lines = Files.readAllLines(Paths.get(output_file_path));
-		assertEquals(expected_lines, lines);
+		// Examine it
+		String actual_content = readFile(output_file_path);
+		assertEquals(expected_content, actual_content);
 
-		// Finally, clean up your mess
+		// Finally, clean up the mess
 		if (s3client.doesObjectExist(bucketName, s3ObjectKey)) {
 			s3client.deleteObject(bucketName, s3ObjectKey);
 		}
@@ -94,23 +102,7 @@ public class AphlS3ComponentTest {
 		assertTrue(!Files.exists(Paths.get(output_file_path)));
 	}
 
-	@Test
-	@DirtiesContext
-	public void testAphlS3Producer() throws Exception {
-		try {
-			Exchange exchange = new DefaultExchange(camelContext);
-			Message msg = new DefaultMessage();
-
-			Map map = new HashMap<>();
-			map.put("CamelAwsS3Key", "arbitraryKey");
-			msg.setHeader("CamelAwsS3Key", "arbitraryKey");
-			msg.setBody(map);
-			exchange.setIn(msg);
-
-			foo.expectedMessageCount(1);
-			template.send(exchange);
-			MockEndpoint.assertIsSatisfied(camelContext);
-		} finally {
-		}
+	private String readFile(String file) throws IOException {
+		return new String(java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(file)));
 	}
 }
