be111ff3ff304a997def6857f46134015420030f
Rob Dingwell <rob@robdingwell.org>
2017-06-07 12:56:55 -0400

    adding safeguards for source_attributes to protect against null pointers during cloning

--- src/main/java/org/cdc/gov/sdp/model/SDPMessage.java
+++ src/main/java/org/cdc/gov/sdp/model/SDPMessage.java
@@ -8,51 +8,65 @@ import org.springframework.beans.BeanUtils;
 
 /**
  * SDP-CBR message
+ * 
  * @author ECOLE
  *
  */
-public class SDPMessage implements Cloneable{
-	
+public class SDPMessage implements Cloneable {
+
 	public final static String SDP_MESSAGE_HEADER = "SDP_MESSAGE_HEADER";
 
 	/**
-	 * Whether the message is a batch message and has been split into individual messages.  
-	 * If it is a batch mehttps://www.amazon.com/Josie-Marks/e/B00YFPK5IIssage and has not been split this will be false.
+	 * Whether the message is a batch message and has been split into individual
+	 * messages. If it is a batch
+	 * mehttps://www.amazon.com/Josie-Marks/e/B00YFPK5IIssage and has not been
+	 * split this will be false.
 	 */
 	private boolean batch;
-	
+
 	/**
-	 * The identifier of the individual batch within a message. If the batch does not contain an identifier this field will be empty.
+	 * The identifier of the individual batch within a message. If the batch
+	 * does not contain an identifier this field will be empty.
 	 */
 	private boolean batch_id;
-	
+
 	/**
-	 * If the message is a batch message and has been split this will contain the index of the message inside the batch. Index position starts at 0. This field has no meaning if the batch property is set to false or is not included
+	 * If the message is a batch message and has been split this will contain
+	 * the index of the message inside the batch. Index position starts at 0.
+	 * This field has no meaning if the batch property is set to false or is not
+	 * included
 	 */
 	private int batch_index;
 
 	/**
-	 * The time that the CBR service received the message from the source.  The format of this will be the same as the source_recieved_time.
+	 * The time that the CBR service received the message from the source. The
+	 * format of this will be the same as the source_recieved_time.
 	 */
 	private String cbr_received_time;
 
 	/**
-	 * The CBR identifier for the message. The general case for this field will be a concatenation of the source, source_id and batch_index if applicable",
-     */
+	 * The CBR identifier for the message. The general case for this field will
+	 * be a concatenation of the source, source_id and batch_index if
+	 * applicable",
+	 */
 	private String id;
-	
+
 	/**
 	 * the message from the source system
 	 */
 	private String payload;
-       
+
 	/**
-	 * If available from the source this will contain the information about the intended recipient of the message.  If this information cannot be derived from the source this field will be null.
+	 * If available from the source this will contain the information about the
+	 * intended recipient of the message. If this information cannot be derived
+	 * from the source this field will be null.
 	 */
 	private String recipient;
-    
+
 	/**
-	 * If available from the source this will contain the information about the sender of the message. If this information cannot be derived  from the source this filed will be null
+	 * If available from the source this will contain the information about the
+	 * sender of the message. If this information cannot be derived from the
+	 * source this filed will be null
 	 */
 	private String sender;
 
@@ -60,19 +74,23 @@ public class SDPMessage implements Cloneable{
 	 * The identifier of the source system that the message was obtained from
 	 */
 	private String source;
-	
+
 	/**
-	 * Name value pairs of additional information provided by the source of the message
+	 * Name value pairs of additional information provided by the source of the
+	 * message
 	 */
-	private Map source_attributes;
-	
+	private Map source_attributes = new HashMap();
+
 	/**
 	 * The identifier of the message from the source system
 	 */
 	private String source_id;
-	
+
 	/**
-	 * The time the message was received by the source system, the value will be formatted as defined by RFC3339, e.g. 1985-04-12T23:20:50.52Z for times in UTC or 1996-12-19T16:39:57-08:00 for times in other time zones.   If this information is not available the field will be null.
+	 * The time the message was received by the source system, the value will be
+	 * formatted as defined by RFC3339, e.g. 1985-04-12T23:20:50.52Z for times
+	 * in UTC or 1996-12-19T16:39:57-08:00 for times in other time zones. If
+	 * this information is not available the field will be null.
 	 */
 	private String source_received_time;
 
@@ -172,22 +190,22 @@ public class SDPMessage implements Cloneable{
 		this.source_received_time = source_received_time;
 	}
 
-
 	public SDPMessage clone() {
-		
+
 		SDPMessage msg = new SDPMessage();
 		BeanUtils.copyProperties(this, msg);
-		// make sure this has a hash of its own so if things change they only effect the single instance and not any of the clones
-		// or vise versa 
+		// make sure this has a hash of its own so if things change they only
+		// effect the single instance and not any of the clones
+		// or vise versa
 		Map newAtts = new HashMap();
-		for (Iterator iterator = source_attributes.keySet().iterator(); iterator.hasNext();) {
-			Object key = iterator.next();
-			newAtts.put(key, source_attributes.get(key));
+		if (source_attributes != null) {
+			for (Iterator iterator = source_attributes.keySet().iterator(); iterator.hasNext();) {
+				Object key = iterator.next();
+				newAtts.put(key, source_attributes.get(key));
+			}
 		}
 		msg.setSourceAttributes(newAtts);
 		return msg;
 	}
 
-	
-	
 }
