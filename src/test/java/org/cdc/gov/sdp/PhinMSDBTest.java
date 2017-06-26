37a20ec386c882549188f551f47f9c4b18eb6b4e
Betsy Cole <ecole@mitre.org>
2017-06-26 09:59:49 -0400

    Adding PhinMS producer.

--- src/test/java/org/cdc/gov/sdp/PhinMSDBTest.java
+++ src/test/java/org/cdc/gov/sdp/PhinMSDBTest.java
@@ -2,12 +2,26 @@ package org.cdc.gov.sdp;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.sql.ResultSet;
 import java.util.List;
 import java.util.Map;
 
 import javax.sql.DataSource;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.impl.DefaultMessage;
 import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
 import org.apache.camel.test.spring.CamelTestContextBootstrapper;
 import org.junit.Test;
@@ -26,26 +40,69 @@ public class PhinMSDBTest {
 	
 	@Autowired
 	protected CamelContext camelContext;
-	
-	private String sql = "select * from message_inq where applicationStatus is NULL and processingStatus = 'queued'";
-	
-	@Test
-	public void testInsertIntoPhinMS() {
-		DataSource ds = (DataSource) camelContext.getRegistry().lookupByName("phinMSDataSource");
-		JdbcTemplate jdbcTemplate = new JdbcTemplate(ds);
-		String query = "INSERT INTO message_inq "
-				+ "(localFileName, service, action, encryption) "
-				+ "values (?, ?, ?, ?)";	
-		int rows = jdbcTemplate.update(query, "localFile1", "service1", "action1", "SHA-128");
-		assertEquals(1, rows);
 
-		List<Map<String,Object>> results = jdbcTemplate.queryForList("select * from message_inq where localFileName='localFile1'");
-		assertEquals(1, results.size());
-		
-		jdbcTemplate.update("delete from message_inq where localFileName='localFile1'");
-		
-		results = jdbcTemplate.queryForList("select * from message_inq where localFileName='localFile1'");
-		assertEquals(0, results.size());
+	@EndpointInject(uri = "mock:phinmsDb")
+	protected MockEndpoint phinMSDb;
+
+	@Produce(uri = "direct:phinms")
+	protected ProducerTemplate template;
+	
+	private final String file_name = "phinms_input.csv";
+	private final String input_file_path = "src/test/resources/" + file_name;
+
+	@Test
+	public void testPhinMSProducer() {
+		JdbcTemplate jdbcTemplate = null;
+		int count = -1;
+		try {
+			DataSource ds = (DataSource) camelContext.getRegistry().lookupByName("phinMSDataSource");
+			jdbcTemplate = new JdbcTemplate(ds);
+			
+			count = jdbcTemplate.queryForObject("select count(*) from message_inq", Integer.class);
+			
+			Exchange exchange = new DefaultExchange(camelContext);
+			Message msg = new DefaultMessage();
+			String expected_content = readFile(input_file_path);
+			msg.setBody(expected_content);
+			exchange.setIn(msg);
+			phinMSDb.expectedMessageCount(1);
+			template.send(exchange);
+			MockEndpoint.assertIsSatisfied(camelContext);
+			int linesInFile = getLinesInFile(input_file_path);
+			
+			int countDelta = jdbcTemplate.queryForObject("select count(*) from message_inq", Integer.class) - count;
+			
+			assertEquals(linesInFile-1, countDelta);
+		} catch (IOException e) {
+			fail("Could not read input file");
+		} catch (InterruptedException e) {
+			fail("Interrupted Exception thrown");
+		} finally {
+			if (jdbcTemplate != null) {
+				jdbcTemplate.execute("DELETE from message_inq where fromPartyId='testPhinMS_Producer'");
+
+				if (count > 0) {
+					assertEquals(count, (int)jdbcTemplate.queryForObject("select count(*) from message_inq", Integer.class));
+				}
+			}
+		}
 	}
 	
+	
+	private String readFile(String file) throws IOException {
+		return new String(java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(file)));
+	}
+	
+	private int getLinesInFile(String file) throws IOException {
+		LineNumberReader lnr = null;
+		try {
+			lnr = new LineNumberReader(new FileReader(new File(file)));
+			lnr.skip(Long.MAX_VALUE);
+			int lines = lnr.getLineNumber();
+			return lines;
+		} finally {
+			if (lnr != null)
+				lnr.close();
+		}
+	}
 }
