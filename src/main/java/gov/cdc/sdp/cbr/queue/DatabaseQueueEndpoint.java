2ad470571da6a17fc4f8234669f68a181e3008e9
Kevin Hennessy <khennessy@mitre.org>
2017-10-04 10:47:40 -0400

    Initial parameter adds for limit and maxAttempts

--- src/main/java/gov/cdc/sdp/cbr/queue/DatabaseQueueEndpoint.java
+++ src/main/java/gov/cdc/sdp/cbr/queue/DatabaseQueueEndpoint.java
@@ -26,95 +26,102 @@ import org.springframework.jdbc.core.RowMapperResultSetExtractor;
 
 @UriEndpoint(scheme = "sdpqueue", title = "SDPQueue", syntax = "sdpqueue", label = "database,sql,queue")
 public class DatabaseQueueEndpoint extends DefaultEndpoint {
-    private static final Logger LOG = LoggerFactory.getLogger(DatabaseQueueEndpoint.class);
+	private static final Logger LOG = LoggerFactory.getLogger(DatabaseQueueEndpoint.class);
 
-    @UriParam(description = "Sets the DataSource to use to communicate with the database.")
-    private DataSource dataSource;
-    @UriParam(description = "Sets the table in which the data will be stored.")
-    private String tableName;
-    @UriParam(description = "Delay (in seconds) between polls.")
-    private int delay;
-    @UriParam(description = "InitialDelay (in seconds) before first poll.")
-    private int initialDelay;
+	@UriParam(description = "Sets the DataSource to use to communicate with the database.")
+	private DataSource dataSource;
+	@UriParam(description = "Sets the table in which the data will be stored.")
+	private String tableName;
+	@UriParam(description = "Delay (in seconds) between polls.")
+	private int delay;
+	@UriParam(description = "InitialDelay (in seconds) before first poll.")
+	private int initialDelay;
+	@UriParam(description = "Limit of results returned by each poll.")
+	private int limit;
+	@UriParam(description = "Maximum times to attempt updating.")
+	private int maxAttempts;
 
-    public DatabaseQueueEndpoint(String uri, Component component, DataSource ds, String tableName, int delay,
-            int initialDelay) {
-        super(uri, component);
-        this.dataSource = ds;
-        this.tableName = tableName;
-        this.delay = delay;
-        this.initialDelay = initialDelay;
-    }
+	public DatabaseQueueEndpoint(String uri, Component component, DataSource ds, String tableName, int delay,
+			int initialDelay, int limit, int maxAttempts) {
+		super(uri, component);
+		this.dataSource = ds;
+		this.tableName = tableName;
+		this.delay = delay;
+		this.initialDelay = initialDelay;
+		this.limit = limit;
+		this.maxAttempts = maxAttempts;
+	}
 
-    @Override
-    public Producer createProducer() throws Exception {
-        return new DatabaseQueueProducer(this, getEndpointUri(), dataSource, tableName);
-    }
+	@Override
+	public Producer createProducer() throws Exception {
+		return new DatabaseQueueProducer(this, getEndpointUri(), dataSource, tableName);
+	}
 
-    @Override
-    public boolean isSingleton() {
-        return false;
-    }
+	@Override
+	public boolean isSingleton() {
+		return false;
+	}
 
-    @Override
-    public Consumer createConsumer(Processor processor) throws Exception {
-        return new DatabaseQueueConsumer(this, dataSource, processor, tableName, delay, initialDelay);
-    }
+	@Override
+	public Consumer createConsumer(Processor processor) throws Exception {
+		return new DatabaseQueueConsumer(this, dataSource, processor, tableName, delay, initialDelay, limit,
+				maxAttempts);
+	}
 
-    private String outputClass = "String";// TODO this wont always be a string,
-                                          // yeah?
+	private String outputClass = "String";// TODO this wont always be a string,
+											// yeah?
 
-    public List<?> queryForList(String[] headers, ResultSet rs, boolean allowMapToClass) throws SQLException {
+	public List<?> queryForList(String[] headers, ResultSet rs, boolean allowMapToClass) throws SQLException {
 
-        ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+		ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
 
-        while (rs.next()) {
-            HashMap<String, String> row = new HashMap<String, String>();
-            for (int i = 0; i < headers.length; i++) {
-                row.put(headers[i], rs.getString(headers[i]));
-            }
-            data.add(row);
-        }
+		while (rs.next()) {
+			HashMap<String, String> row = new HashMap<String, String>();
+			for (int i = 0; i < headers.length; i++) {
+				row.put(headers[i], rs.getString(headers[i]));
+			}
+			data.add(row);
+		}
 
-        return data;
-    }
+		return data;
+	}
 
-    @SuppressWarnings("unchecked")
-    public Object queryForObject(ResultSet rs) throws SQLException {
-        Object result = null;
-        if (outputClass == null) {
-            RowMapper rowMapper = new ColumnMapRowMapper();
-            RowMapperResultSetExtractor<Map<String, Object>> mapper = new RowMapperResultSetExtractor<Map<String, Object>>(
-                    rowMapper);
-            List<Map<String, Object>> data = mapper.extractData(rs);
-            if (data.size() > 1) {
-                LOG.error("Query result not unique for outputType=SelectOne. Got " + data.size() + " count instead.");
-                throw new SQLDataException(
-                        "Query result not unique for outputType=SelectOne. Got " + data.size() + " count instead.");
-            } else if (data.size() == 1) {
-                // Set content depend on number of column from query result
-                Map<String, Object> row = data.get(0);
-                if (row.size() == 1) {
-                    result = row.values().iterator().next();
-                } else {
-                    result = row;
-                }
-            }
-        } else {
-            Class<?> outputClzz = getCamelContext().getClassResolver().resolveClass(outputClass);
-            RowMapper rowMapper = new BeanPropertyRowMapper(outputClzz);
-            RowMapperResultSetExtractor<?> mapper = new RowMapperResultSetExtractor(rowMapper);
-            List<?> data = mapper.extractData(rs);
-            if (data.size() > 1) {
-                LOG.error("Query result not unique for outputType=SelectOne. Got " + data.size() + " count instead.");
-                throw new SQLDataException(
-                        "Query result not unique for outputType=SelectOne. Got " + data.size() + " count instead.");
-            } else if (data.size() == 1) {
-                result = data.get(0);
-            }
-        }
+	@SuppressWarnings("unchecked")
+	public Object queryForObject(ResultSet rs) throws SQLException {
+		Object result = null;
+		if (outputClass == null) {
+			RowMapper rowMapper = new ColumnMapRowMapper();
+			RowMapperResultSetExtractor<Map<String, Object>> mapper = new RowMapperResultSetExtractor<Map<String, Object>>(
+					rowMapper);
+			List<Map<String, Object>> data = mapper.extractData(rs);
+			if (data.size() > 1) {
+				LOG.error("Query result not unique for outputType=SelectOne. Got " + data.size() + " count instead.");
+				throw new SQLDataException(
+						"Query result not unique for outputType=SelectOne. Got " + data.size() + " count instead.");
+			} else if (data.size() == 1) {
+				// Set content depend on number of column from query result
+				Map<String, Object> row = data.get(0);
+				if (row.size() == 1) {
+					result = row.values().iterator().next();
+				} else {
+					result = row;
+				}
+			}
+		} else {
+			Class<?> outputClzz = getCamelContext().getClassResolver().resolveClass(outputClass);
+			RowMapper rowMapper = new BeanPropertyRowMapper(outputClzz);
+			RowMapperResultSetExtractor<?> mapper = new RowMapperResultSetExtractor(rowMapper);
+			List<?> data = mapper.extractData(rs);
+			if (data.size() > 1) {
+				LOG.error("Query result not unique for outputType=SelectOne. Got " + data.size() + " count instead.");
+				throw new SQLDataException(
+						"Query result not unique for outputType=SelectOne. Got " + data.size() + " count instead.");
+			} else if (data.size() == 1) {
+				result = data.get(0);
+			}
+		}
 
-        // If data.size is zero, let result be null.
-        return result;
-    }
+		// If data.size is zero, let result be null.
+		return result;
+	}
 }
