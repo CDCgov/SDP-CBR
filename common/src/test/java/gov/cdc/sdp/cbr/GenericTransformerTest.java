c85db5e40e73f440a182202b9b5036977547d89b
Betsy Cole <ecole@mitre.org>
2017-12-27 14:14:25 -0500

    First pass at the addition of a REST API.
    Contains a new restapi subproject, with a single controller which
    receives input requests and dispatches them to an endpoint as valid hl7
    input messages.
    
    Also contains updated tests and inputs for the common package designed
    to receive the requests sent by the REST API and split them into batch
    messages for processing.

--- common/src/test/java/gov/cdc/sdp/cbr/GenericTransformerTest.java
+++ common/src/test/java/gov/cdc/sdp/cbr/GenericTransformerTest.java
@@ -1,5 +1,8 @@
 package gov.cdc.sdp.cbr;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -15,6 +18,7 @@ import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.ExchangeBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.impl.DefaultExchange;
 import org.apache.camel.impl.DefaultMessage;
@@ -24,10 +28,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(CamelSpringJUnit4ClassRunner.class)
 @BootstrapWith(CamelTestContextBootstrapper.class)
@@ -43,43 +50,39 @@ public class GenericTransformerTest {
     
     @Produce(uri = "direct:start")
     protected ProducerTemplate template;
-
+    
     @Test
-    @DirtiesContext
-    public void testGenericProcessor() throws Exception {
-    	mock_endpoint.reset();
+    public void testWithInputFromRest() throws InterruptedException, IOException {
+        mock_endpoint.reset();
         
         String source_file = "src/test/resources/BatchTest_GenV2_2msgs.txt";
-
-        Exchange exchange = new DefaultExchange(camelContext);
-        Message msg = new DefaultMessage();
-
-        Map<String, String> map = new HashMap<>();
-        map.put("recordId", "testGenericProcessor_rec");
-        map.put("sourceId", "testGenericProcessor_src");
-        map.put("messageId", "testGenericProcessor_msg");
-        map.put("payloadName", "Name");
-        map.put("payloadBinaryContent", readFile(source_file));
-        map.put("payloadTextContent", readFile(source_file));
-        map.put("localFileName", "file??");
-        map.put("service", "service");
-        map.put("action", "action");
-        map.put("arguments", "arge");
-        map.put("fromPartyId", "testGenericProcessor");
-        map.put("messageRecipient", "recipient");
-        map.put("receivedTime", new Date().toString());
-        msg.setBody(map);
         
-        msg.setHeader("FILTER", true);
-
-        exchange.setIn(msg);
-
-        mock_endpoint.expectedMessageCount(1);
-        mock_endpoint.expectedHeaderReceived("CBR_ID", "_ROUTE1_testGenericProcessor_src");
-        mock_endpoint.expectedHeaderReceived("sourceId", "testGenericProcessor_src");
+        MockMultipartFile file = new MockMultipartFile(
+                "file", 
+                "hello.txt",
+                MediaType.TEXT_PLAIN_VALUE, 
+                java.nio.file.Files.readAllBytes(java.nio.file.Paths.get(source_file)));
         
+        Exchange ex = new ExchangeBuilder(camelContext)
+                .withBody(file)
+                .withHeader("CBR_ID", "CBR_testSrc_test")
+                .withHeader("ORIGINATING_CBR_ID", "CBR_testSrc_test")
+                .withHeader("sourceId", "testSrc")
+                .withHeader("METADATA", new HashMap<String,String>()).build();
         
-        template.send(exchange);
+        mock_endpoint.expectedMessageCount(3);
+        mock_endpoint.expectedHeaderValuesReceivedInAnyOrder(
+                "CBR_ID", 
+                "CBR_testSrc_test_0", 
+                "CBR_testSrc_test_1", 
+                "CBR_testSrc_test_2");
+        
+        mock_endpoint.expectedHeaderReceived(
+                "ORIGINATING_CBR_ID", 
+                "CBR_testSrc_test");
+        
+        mock_endpoint.expectedHeaderReceived("sourceId", "testSrc");
+        template.send(ex);
 
         MockEndpoint.assertIsSatisfied(camelContext);
     }
